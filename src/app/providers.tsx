67e23bfe412dedbe22ef43f7f8dfb6b0145a416f
met43211 <matvey-anashkin@yandex.ru>
2025-01-31 00:21:59 +0300

    fix: lazy motion

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,10 @@
-import { LazyMotion } from 'motion/react';
+import { domMax, LazyMotion } from 'motion/react';
 import { PropsWithChildren } from 'react';
-
-const loadFeatures = () => import('@/shared/model/dom-max').then((res) => res.domMax);
 
 export const Providers = ({ children }: PropsWithChildren) => {
   return (
     <>
-      <LazyMotion features={loadFeatures}>{children}</LazyMotion>
+      <LazyMotion features={domMax}>{children}</LazyMotion>
     </>
   );
 };
