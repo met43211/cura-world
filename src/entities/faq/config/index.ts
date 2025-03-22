cfe16cff605bd09d58037a0a52fe85cbc9a1d84c
met43211 <matvey-anashkin@yandex.ru>
2025-03-22 15:39:22 +0300

    fix: faq answers

--- src/entities/faq/config/index.ts
+++ src/entities/faq/config/index.ts
@@ -14,7 +14,7 @@ export const faqData: FAQData[] = [
     id: 2,
     question: 'How do I get a whitelist for the next gene?',
     answer:
-      '- Yes, we plan to start working on the next gen after the game launches. We need this to expand our community.',
+      ' You must be a genesis collection holder or participate in our game. Information about this will be available later.',
   },
   {
     id: 3,
