ccb18f4281e56eed5a261461ed842f9c1ae36ac1
met43211 <matvey-anashkin@yandex.ru>
2025-03-23 14:58:42 +0300

    debug: logs in login

--- src/features/login/ui/index.tsx
+++ src/features/login/ui/index.tsx
@@ -13,13 +13,19 @@ export const Login = () => {
   const handleClick = async () => {
     const { code_challenge, code_verifier } = await pkceChallenge();
 
+    console.log(code_challenge, code_verifier);
+
     Cookies.set('code_verifier', code_verifier);
 
     const url = process.env.NEXT_PUBLIC_AUTH_URL;
     const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
     const redirectUrl = process.env.NEXT_PUBLIC_REDIRECT_URL;
 
+    console.log(url, clientId, redirectUrl);
+
     const authUrl = `${url}?client_id=${clientId}&scope=profile nfts socials.discord socials.twitter wallets&response_type=code&redirect_uri=${redirectUrl}&code_challenge=${code_challenge}&code_challenge_method=S256&state=x1561axa165`;
+
+    console.log(authUrl);
 
     router.push(authUrl);
   };
